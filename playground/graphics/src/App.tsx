6089ed36120befef36b7e2e483057e6132a8bf64
designDefined <designdefined.by@gmail.com>
2024-10-24 17:35:36 +0900

    feat: Setup test environment for svg filter on text

--- playground/graphics/src/App.tsx
+++ playground/graphics/src/App.tsx
@@ -1,5 +1,18 @@
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <main className="fullscreen">
+      <div className="background sky-gradient-08"></div>
+      <div className="foreground">
+        <svg className="svg" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
+          <text className="title" x="100" y="50" textAnchor="middle">
+            THE CODEC
+          </text>
+        </svg>
+      </div>
+    </main>
+  );
 }
 
 export default App;
