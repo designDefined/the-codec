7db7cac8370cbd407d740765fe7b93058c841792
designDefined <designdefined.by@gmail.com>
2025-01-25 17:59:53 +0900

    feat: Apply base theme color to index editor and soften look of panel handle

--- service/app/src/ui-editor/idx/IndexPage/index.tsx
+++ service/app/src/ui-editor/idx/IndexPage/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 import { useIndexId } from "../../../router/local/useResourceId";
 import { useIntent, useView } from "viajs-react";
 import { IndexInformationEditor } from "@module/index/IndexInformationEditor";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IndexIntent } from "@core/intent/index/index";
 import { IndexView } from "@core/view/index";
 import { MainContent } from "@component/area";
@@ -14,8 +14,19 @@ import { Box } from "@core/entity/box/Box";
 import { produce } from "immer";
 import { ContentEditorContext, SelectedContentEditor, useContentEditorRoot } from "@module/content";
 import { MinimizableSection } from "./MinimizableSection";
+import { useThemeStyle } from "@module/box/look";
 
 const cx = bindCSS(styles);
+
+/**
+ * TODO: Isolate to separate module
+ */
+const beforeUnloadHandler: EventListener = event => {
+  // Recommended
+  event.preventDefault();
+  // Included for legacy support, e.g. Chrome/Edge < 119
+  event.returnValue = true;
+};
 
 export const IndexPage = () => {
   const id = useIndexId();
@@ -39,10 +50,17 @@ export const IndexPage = () => {
     boxEditorContext.selected?.box.type === "INNER_BOX" ? boxEditorContext.selected.box.id : undefined,
   );
 
+  const themeStyle = useThemeStyle(index.content.look.theme);
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", beforeUnloadHandler);
+    return () => window.removeEventListener("beforeunload", beforeUnloadHandler);
+  }, []);
+
   return (
     <BoxEditorContext.Provider value={boxEditorContext}>
       <ContentEditorContext.Provider value={contentEditorContext}>
-        <Main className={cx("IndexPage")} row sizeC="100vh" hide>
+        <Main className={cx("IndexPage")} row sizeC="100vh" hide style={themeStyle}>
           <Section className={cx("contentSection")} f overM py={128}>
             <MainContent
               onClick={() => boxEditorContext.clearSelected()}
@@ -50,31 +68,18 @@ export const IndexPage = () => {
             >
               <BoxEditor box={index.content} path={[{ id: index.content.id, name: index.content.name }]} />
             </MainContent>
-          </Section>
-          <Section className={cx("panelSection")} basis={isPanelOpen ? 520 : 40} row sizeM={"100vh"} hide>
-            <Div className={cx("handle")} basis={40} alignM>
-              <Button
-                className={cx("panelButton")}
-                onClick={() => send({ index }).then(() => setModified(false))}
-                disabled={!modified}
-                px={0}
-                py={8}
-                alignC
-                rad={8}
-              >
-                저장
+            <Div className={cx("handle")} fixed top={0} right={isPanelOpen ? 480 : 0} sizeM="100vh">
+              <Button onClick={() => send({ index }).then(() => setModified(false))} disabled={!modified} alignC p={8}>
+                Save{modified ? "?" : "d"}
               </Button>
-              <Button
-                className={cx("panelButton")}
-                onClick={() => setIsPanelOpen(prev => !prev)}
-                f
-                alignC
-                alignM
-                rad={8}
-              >
-                <Chevron className={cx("icon")} reversed={!isPanelOpen} />
-              </Button>
+              <Div f alignM alignC="end">
+                <Button onClick={() => setIsPanelOpen(prev => !prev)} px={8} py={24}>
+                  <Chevron className={cx("icon")} size={16} reversed={!isPanelOpen} />
+                </Button>
+              </Div>
             </Div>
+          </Section>
+          <Section className={cx("panelSection")} basis={isPanelOpen ? 480 : 0} row sizeM={"100vh"} hide>
             <Article className={cx("panels")} basis={480} hideC overM="scroll">
               <Div p={24} overM hideC>
                 <MinimizableSection name="Index">
